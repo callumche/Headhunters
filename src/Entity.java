aac562b16b27a4672b4ec37376a7ad1842cc4182
PinguPenguin <jub.jub.madej@gmail.com>
2023-05-01 14:21:28 -0400

    Refactored speed cap + added deceleration

--- src/Entity.java
+++ src/Entity.java
@@ -9,6 +9,7 @@ public class Entity {
     private double xa = 0;
     private double ya = 0;
     private boolean inputting = false; //if a key is currently being held down
+    private int speedCap = 15;
 
     public void keyPressed(KeyEvent e){
         if (e.getKeyCode() == KeyEvent.VK_LEFT){
@@ -43,41 +44,58 @@ public class Entity {
     }
 
     public void move() {
-        if (xv < 10 && xv > -10) { //speed cap of 10
+        if (xv < speedCap && xv > -speedCap) { //speed cap of 10
             xv += xa;
-        } else if (xv >= 10) { //if above speed cap, decelerate
+        } else if (xv >= speedCap) { //if above speed cap, decelerate
             xv -= 1;
-        } else if (xv <= -10) { //backwards speed cap
+        } else if (xv <= -speedCap) { //backwards speed cap
             xv += 1;
         }
-        if (yv < 10 && yv > -10) {
+        if (yv < speedCap && yv > -speedCap) {
             yv += ya;
-        } else if (yv >= 10) { //if above speed cap, decelerate
+        } else if (yv >= speedCap) { //if above speed cap, decelerate
             yv -= 1;
-        } else if (yv <= -10) { //backwards speed cap
+        } else if (yv <= -speedCap) { //backwards speed cap
             yv += 1;
         }
         x += xv;
         //below are bounds checks (see if in bounds, if not, reset position to border and invert velocity
         if (x > 1000) { //right border
             x = 999;
-            xv = -1 * xv; //stop when you hit the border
+            xv = -1 * xv + 1; //stop when you hit the border
             System.out.println("right bounce");
         } else if (x < 0) {
             x = 1;
-            xv = -1 * xv;
+            xv = -1 * xv - 1;
             System.out.println("left bounce");
         }
         y -= yv;
         if (y > 590) { //height of windows bar ~= 50px
             y = 589;
-            yv = -1 * yv;
+            yv = -1 * yv - 1;
             System.out.println("bottom bounce");
         } else if (y < 0) {
             y = 1;
-            yv = -1 * yv;
+            yv = -1 * yv + 1;
             System.out.println("top bounce");
         }
+
+        //friction, constantly decelerating at 0.1
+        if (xv > 0.1) {
+            xv -= 0.1;
+        } else if (xv < -0.1) {
+            xv += 0.1;
+        } else {
+            xv = 0;
+        }
+        if (yv > 0.1) {
+            yv -= 0.1;
+        } else if (yv < -0.1) {
+            yv += 0.1;
+        } else {
+            yv = 0;
+        }
+
     }
 
     public void paint (Graphics2D g2d) {
