2806722c3bede0c005a076dc7a87d573ad2acb7e
PinguPenguin <jub.jub.madej@gmail.com>
2023-05-01 12:05:15 -0400

    Attempting to add key inputs

--- src/Entity.java
+++ src/Entity.java
@@ -1,3 +1,53 @@
-public class Entity {
+import java.awt.*;
+import java.awt.event.KeyEvent;
 
+public class Entity {
+    private int x = 300;
+    private int y = 300;
+    private double xv = 0;
+    private double yv = 0;
+    private double xa = 0;
+    private double ya = 0;
+
+    public void keyPressed(KeyEvent e){
+        if (e.getKeyCode() == KeyEvent.VK_LEFT){
+            xa = -1;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+            xa = 1;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_UP){
+            ya = 1;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN){
+            ya = -1;
+        }
+    }
+    public void keyReleased(KeyEvent e){
+        if (e.getKeyCode() == KeyEvent.VK_LEFT){
+            xa = 0;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+            xa = 0;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_UP){
+            ya = 0;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN){
+            ya = 0;
+        }
+    }
+
+    public void updateMovement() {
+        x += xv;
+        y -= yv;
+
+        xv += xa;
+        yv += ya;
+    }
+
+    public void paint (Graphics2D g2d) {
+        g2d.fillRect(x, y, 20, 20);
+        updateMovement();
+    }
 }
--- src/Window.java
+++ src/Window.java
@@ -1,20 +1,43 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class Window extends JPanel {
-    public Window(){
+    Entity e1 = new Entity();
 
+    public Window(){
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+            @Override
+            public void keyPressed(KeyEvent e) {
+            }
+        });
     }
 
     public void paint(Graphics g){
         Graphics2D g2d = (Graphics2D) g;
-
+        e1.paint(g2d);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         JFrame frame = new JFrame("HeadHunters");
         Window win = new Window();
         frame.add(win);
+        frame.setSize(1020, 640);
+        frame.setVisible(true);
+        frame.setResizable(true);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        while(true){
+            Thread.sleep(16);
+            frame.repaint();
+        }
     }
 }
