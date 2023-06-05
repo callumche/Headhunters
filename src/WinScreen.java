4d4e8a74da861e92b12baf482ebe91fdb952fb6b
callumscrums <132367274+callumscrums@users.noreply.github.com>
2023-06-05 11:00:34 -0400

    Added Visual to see if my code works

--- src/WinScreen.java
+++ src/WinScreen.java
@@ -7,6 +7,10 @@ public class WinScreen {
     }
     public void paint(Graphics2D g2d){
         p1Winner = Window.getWinner();
-
+        if (p1Winner){
+            g2d.drawString("Player 1 win",500,500);
+        } else {
+            g2d.drawString("Player 2 win",500,500);
+        }
     }
 }
