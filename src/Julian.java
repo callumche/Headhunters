27ce201f65c11a15e4793b9d72df776e585cee72
Naufill <s201118466@ddsbstudent.ca>
2023-06-12 23:57:02 -0400

    First part of Julian's super, where he leaps up and aims his divebomb. Still need to implement the strike.

--- src/Julian.java
+++ src/Julian.java
@@ -1,14 +1,40 @@
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class Julian extends Character{
     private boolean isInitialized = false;
 
+    private BufferedImage crosshair;
+
     public Julian (boolean p) {
         super(p);
         charType = 0;
+    }
+
+    public void special() {
+        if (attackState == 0 && specialCount == 100) {
+            specialCount = 0;
+            attackState = 5;
+            markerFrame = Window.getTick();
+            changeImage("special1");
+        }
+
+        if (attackState == 5 && Window.getTick() - markerFrame >= 30) {
+
+            if (y >= -200) {
+                yv = 10;
+                move();
+
+                changeImage("bite1");
+
+            } else {
+                yv = 0;
+            }
+
+        }
     }
 
     public void paint(Graphics2D g2d){
@@ -25,6 +51,7 @@ public class Julian extends Character{
                 spit1 = ImageIO.read(new File("res//Sprites//JulianSpit1.PNG"));
                 spit2 = ImageIO.read(new File("res//Sprites//JulianSpit2.PNG"));
                 evil = ImageIO.read(new File("res//Sprites//JulianEvil.PNG"));
+                crosshair = ImageIO.read(new File("res//Crosshair.PNG"));
             } catch (IOException e) {
                 System.out.println("Missing Julian Image: " + e);
             }
@@ -40,5 +67,12 @@ public class Julian extends Character{
             g2d.drawImage(current, x + Window.xOffset, y + Window.yOffset, current.getWidth(), current.getHeight(), null);
         }
         move();
+
+        if (attackState == 5 && Window.getTick() - markerFrame >= 30) {
+
+            g2d.drawImage(crosshair, x, Window.resY - 150, 100, 100, null);
+
+        }
+
     }
 }
